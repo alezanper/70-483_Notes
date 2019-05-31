de125173547365ad2197019d75262d359523168d
alezanper <fabenavideso@gmail.com>
2019-05-30 20:55:02 -0500

    skill 1.1

--- Skill 1_1 Implement multithreading and asynchronous/parallel/parallel/Program.cs
+++ Skill 1_1 Implement multithreading and asynchronous/parallel/parallel/Program.cs
@@ -7,30 +7,48 @@ namespace parallel
 {
     class Program
     {
-        static void Task(string taskName)
+        class Task
         {
-            Console.WriteLine(taskName + " starting");
-            Thread.Sleep(500);
-            Console.WriteLine(taskName + " ending");
+            public string Name { get; set; }
+            public int Time { get; set; }
+
+            public void Launch()
+            {
+                Console.WriteLine(this.Name + " starting");
+                Thread.Sleep(this.Time);
+                Console.WriteLine(this.Name + " ending");
+            }
         }
  
         static void UsingForEach()
         {
+            Task[] tasks = new Task[]
+            {
+                new Task { Name = "Task 1", Time = 500},
+                new Task { Name = "Task 2", Time = 500},
+                new Task { Name = "Task 3", Time = 500},
+                new Task { Name = "Task 4", Time = 500},
+                new Task { Name = "Task 5", Time = 500}
+            };
+
             var items = Enumerable.Range(0, 100);
 
-            Parallel.ForEach(items, item =>
+            Parallel.ForEach(tasks, task =>
             {
-                Console.WriteLine("Started working on: " + item);
-                Thread.Sleep(50);
-                Console.WriteLine("Finished working on: " + item);
+                Console.WriteLine("Starting " + task.Name);
+                Thread.Sleep(task.Time);
+                Console.WriteLine("Ending " + task.Name);
             });
         }
 
         static void Main(string[] args)
         {
+            Task t1 = new Task { Name = "Task 1", Time = 500 };
+            Task t2 = new Task { Name = "Task 2", Time = 500 };
+
             UsingForEach();
 
-            Parallel.Invoke(() => Task("Task 1"), () => Task("Task 2"));
+            Parallel.Invoke(() => t1.Launch(), () => t2.Launch());
 
             Console.ReadKey();
         }
