5f940360398668b203aca62fbf2f8e4f21145792
alezanper <fabenavideso@gmail.com>
2019-05-30 22:34:31 -0500

    skill 1.1

--- Skill 1_1 Implement multithreading and asynchronous/parallel/parallel/Program.cs
+++ Skill 1_1 Implement multithreading and asynchronous/parallel/parallel/Program.cs
@@ -19,37 +19,74 @@ namespace parallel
                 Console.WriteLine(this.Name + " ending");
             }
         }
- 
-        static void UsingForEach()
-        {
-            Task[] tasks = new Task[]
+
+        static Task[] tasks = new Task[]
             {
                 new Task { Name = "Task 1", Time = 500},
                 new Task { Name = "Task 2", Time = 500},
                 new Task { Name = "Task 3", Time = 500},
                 new Task { Name = "Task 4", Time = 500},
-                new Task { Name = "Task 5", Time = 500}
+                new Task { Name = "Task 5", Time = 500},
+                new Task { Name = "Task 6", Time = 500},
+                new Task { Name = "Task 7", Time = 500},
+                new Task { Name = "Task 8", Time = 500},
+                new Task { Name = "Task 9", Time = 500},
+                new Task { Name = "Task 10", Time = 500}
             };
 
-            var items = Enumerable.Range(0, 100);
+        static void SimpleInvoke()
+        {
+            Parallel.Invoke(() => tasks[0].Launch(), () => tasks[1].Launch());
+        }
 
+        static void UsingForEach()
+        {        
             Parallel.ForEach(tasks, task =>
             {
-                Console.WriteLine("Starting " + task.Name);
-                Thread.Sleep(task.Time);
-                Console.WriteLine("Ending " + task.Name);
+                task.Launch();
             });
+        }
+
+        static void UsingFor()
+        {
+            Parallel.For(0, tasks.Length, i =>
+            {
+                tasks[i].Launch();
+            });
+        }
+
+        static void ControlLoop()
+        {
+            ParallelLoopResult result = Parallel.For(0, tasks.Count(), (int i, ParallelLoopState loopState) =>
+            {
+                if (i == 2)
+                    loopState.Stop();
+                tasks[i].Launch();
+            });
+
+            Console.WriteLine("Completed: " + result.IsCompleted);
+        }
+
+        static void UsingLinq()
+        {
+            var result = from task in tasks.AsParallel()
+                             //.AsOrdered()     Para 
+                             //.WithDegreeOfParallelism(4)
+                             //.WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                         where task.Name.Equals("Task 6")
+                         select task;
+
+            foreach (var task in result)
+                Console.WriteLine(task.Name);
         }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task { Name = "Task 1", Time = 500 };
-            Task t2 = new Task { Name = "Task 2", Time = 500 };
-
+            SimpleInvoke();
             UsingForEach();
-
-            Parallel.Invoke(() => t1.Launch(), () => t2.Launch());
-
+            UsingFor();
+            ControlLoop();
+            UsingLinq();
             Console.ReadKey();
         }
     }
