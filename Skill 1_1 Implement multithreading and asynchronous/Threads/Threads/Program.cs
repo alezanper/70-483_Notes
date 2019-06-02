8496c3c3d1a4479b18d946488dba2d4623d1e6d6
alezanper <fabenavideso@gmail.com>
2019-06-01 21:16:33 -0500

    skill 1.1 Threads

---
+++ Skill 1_1 Implement multithreading and asynchronous/Threads/Threads/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Threads
+{
+    class Program
+    {
+        static void UsingThreads()
+        {
+            Thread myThread = new Thread(SomeCode);
+            myThread.Start();
+        }
+
+        static void SomeCode()
+        {
+            Console.WriteLine("Something");
+        }
+
+        static void UsingLambdaWithThreads()
+        {
+            Thread myThread = new Thread(() =>
+            {
+                Console.WriteLine("Something from Lambda");
+            });
+
+            myThread.Start();
+        }
+
+        static void PassingDataToThreads()
+        {
+            Thread myThread = new Thread((data) =>
+            {
+                Console.WriteLine("Data: " + data);
+            });
+
+            myThread.Start("inserted");
+        }
+
+        static void AbortingThreads()
+        {
+            bool tickRunning;
+            Thread myThread = new Thread(() =>
+            {
+                tickRunning = true;
+                while (tickRunning)
+                {
+                    Console.WriteLine("Looping");
+                    Thread.Sleep(1000);
+                }
+            });
+
+            myThread.Start();
+            Console.WriteLine("Press a key to stop the loop");
+            Console.ReadKey();
+            tickRunning = false;
+            //myThread.Abort();
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            UsingThreads();
+            UsingLambdaWithThreads();
+            PassingDataToThreads();
+            AbortingThreads();
+            Console.ReadKey();
+        }
+    }
+}
